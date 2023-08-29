6045c53e1a5b4e5f8765e1af030cb7a136117724
Amulya Parab <amulyaparab101@gmail.com>
2023-08-29 11:34:16 +0530

    feat: added geolocation and marker to map

--- src/Components/MapBox.js
+++ src/Components/MapBox.js
@@ -4,27 +4,31 @@ import { useEffect } from "react";
 export const MapBox = () => {
   const accessToken =
     "pk.eyJ1IjoiYW1lczI3MDAiLCJhIjoiY2xscm9rbGdhMHRndTNocHM5ZTJqZDc4MiJ9.XWi7h4ohG2YIbhjR3Li6lg";
-  //     const initialViewport = {
-  //       latitude: 37.7577,
-  //       longitude: -122.4376,
-  //       zoom: 10,
-  //     };
-  //       const [viewport, setViewport] = useState(initialViewport);
-
-  //       const handleViewportChange = (newViewport) => {
-  //         setViewport(newViewport);
-  //       };
 
   useEffect(() => {
     mapboxgl.accessToken = accessToken;
     const map = new mapboxgl.Map({
       container: "map-container",
-      style: "mapbox://styles/mapbox/streets-v11",
+      style: "mapbox://styles/mapbox/streets-v12",
       center: [73.8567, 18.5204],
       zoom: 10,
     });
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav, "top-right");
+    map.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true,
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
+    const marker1 = new mapboxgl.Marker()
+      .setLngLat([73.8562, 18.5204])
+      .addTo(map);
+    map.addControl(new mapboxgl.ScaleControl());
+
     return () => map.remove();
   }, []);
   return (
