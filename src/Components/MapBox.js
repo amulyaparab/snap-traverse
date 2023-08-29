c1736fb9c79bcd42f822625188b14518cdf3e8ae
Amulya Parab <amulyaparab101@gmail.com>
2023-08-29 17:57:19 +0530

    feat: stored a screenshot in an image

--- src/Components/MapBox.js
+++ src/Components/MapBox.js
@@ -1,24 +1,26 @@
-import mapboxgl from "mapbox-gl";
+// import mapboxgl from "mapbox-gl";
 import { useEffect, useRef, useState } from "react";
-import ReactMapGl from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Map from "react-map-gl";
+
 export const MapBox = () => {
   const map = useRef(null);
-  const mapContainer = useRef(null);
-  const [mapParameters, setMapParameters] = useState({
-    lon: -122.3293,
-    lat: 37.5516,
-    zoom: 17.38,
-    width: 300,
-    height: 200,
+  const [viewPort, setViewPort] = useState({
+    width: "100%",
+    height: "100%",
+    longitude: 73.8562,
+    latitude: 18.5204,
+    zoom: 10,
   });
-
+  const [imageURL, setImageURL] = useState("");
   const accessToken = process.env.REACT_APP_MAP_TOKEN;
 
-  const [imageURL, setImageURL] = useState("");
+  console.log(viewPort);
   const fetchImage = async () => {
     const res = await fetch(
-      `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${mapParameters.lon},${mapParameters.lat},${mapParameters.zoom},0/${mapParameters.width}x${mapParameters.height}?access_token=${accessToken}`
+      `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${viewPort.longitude},${viewPort.latitude},${viewPort.zoom},0/300x300?access_token=${accessToken}`
     );
+
     if (res.status === 200 || res.status === 201) {
       const data = await res.arrayBuffer();
       const base64Data = btoa(
@@ -27,54 +29,64 @@ export const MapBox = () => {
           ""
         )
       );
-      console.log(map?.current?.getCenter());
-      const { lng, lat } = map?.current?.getCenter();
-      const zoom = map.current.getZoom();
-      console.log("Longitude:", lng);
-      console.log("Latitude:", lat);
-      console.log("Zoom:", zoom);
       const objectURL = `data:image/jpeg;base64,${base64Data}`;
       setImageURL(objectURL);
     } else {
       console.error("Error fetching image data");
     }
   };
-  useEffect(() => {
-    mapboxgl.accessToken = accessToken;
-    const mapBox = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [mapParameters.lon, mapParameters.lat],
-      zoom: 10,
-    });
-    if (!map.current) {
-      map.current = mapBox;
-    }
-    const nav = new mapboxgl.NavigationControl();
-    mapBox.addControl(nav, "top-right");
-    mapBox.addControl(
-      new mapboxgl.GeolocateControl({
-        positionOptions: {
-          enableHighAccuracy: true,
-        },
-        trackUserLocation: true,
-        showUserHeading: true,
-      })
-    );
-    const marker1 = new mapboxgl.Marker()
-      .setLngLat([73.8562, 18.5204])
-      .addTo(mapBox);
-    mapBox.addControl(new mapboxgl.ScaleControl());
-    console.log(map, mapContainer);
-    return () => mapBox.remove();
-  }, []);
   return (
-    <>
-      <div ref={mapContainer} id="map-container" className="map">
-        {/* <ReactMapGl></ReactMapGl> */}
-      </div>
+    <div id="map-container" className="map">
+      <Map
+        onMove={(event) => setViewPort(event.viewState)}
+        {...viewPort}
+        mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN}
+        mapStyle="mapbox://styles/ames2700/cllw50r3000g901pj638y82b0"
+      ></Map>
       <button onClick={() => fetchImage()}>Take a Screenshot</button>
       {imageURL && <img src={imageURL} alt="API Fetched" />}
-    </>
+    </div>
   );
 };
+
+//   useEffect(() => {
+//     mapboxgl.accessToken = accessToken;
+//     const mapBox = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: "mapbox://styles/mapbox/streets-v12",
+//       center: [viewPort.longitude, viewPort.latitude],
+//       zoom: viewPort.zoom,
+//     });
+//     if (!map.current) {
+//       map.current = mapBox;
+//     }
+//     const nav = new mapboxgl.NavigationControl();
+//     mapBox.addControl(nav, "top-right");
+//     mapBox.addControl(
+//       new mapboxgl.GeolocateControl({
+//         positionOptions: {
+//           enableHighAccuracy: true,
+//         },
+//         trackUserLocation: true,
+//         showUserHeading: true,
+//       })
+//     );
+//     const marker1 = new mapboxgl.Marker()
+//       .setLngLat([73.8562, 18.5204])
+//       .addTo(mapBox);
+//     mapBox.addControl(new mapboxgl.ScaleControl());
+//     console.log(map, mapContainer);
+//     map.current.on("move", () => {
+//       setViewPort({
+//         ...viewPort,
+//         longitude: map.current.getCenter().lng.toFixed(4),
+//       });
+//       setViewPort({
+//         ...viewPort,
+//         latitude: map.current.getCenter().lat.toFixed(4),
+//       });
+//       setViewPort({ ...viewPort, zoom: map.current.getZoom().toFixed(2) });
+//     });
+//     console.log(viewPort);
+//     return () => mapBox.remove();
+//   }, []);
