3646ca03280fd9a8fa64dd45cc1f3e6c2a7b0436
Amulya Parab <amulyaparab101@gmail.com>
2023-08-29 15:20:37 +0530

    refactor: privatized access token

--- src/Components/MapBox.js
+++ src/Components/MapBox.js
@@ -1,21 +1,58 @@
 import mapboxgl from "mapbox-gl";
-import { useEffect } from "react";
-
+import { useEffect, useRef, useState } from "react";
+import ReactMapGl from "react-map-gl";
 export const MapBox = () => {
-  const accessToken =
-    "pk.eyJ1IjoiYW1lczI3MDAiLCJhIjoiY2xscm9rbGdhMHRndTNocHM5ZTJqZDc4MiJ9.XWi7h4ohG2YIbhjR3Li6lg";
+  const map = useRef(null);
+  const mapContainer = useRef(null);
+  const [mapParameters, setMapParameters] = useState({
+    lon: -122.3293,
+    lat: 37.5516,
+    zoom: 17.38,
+    width: 300,
+    height: 200,
+  });
 
+  const accessToken = process.env.REACT_APP_MAP_TOKEN;
+
+  const [imageURL, setImageURL] = useState("");
+  const fetchImage = async () => {
+    const res = await fetch(
+      `https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/${mapParameters.lon},${mapParameters.lat},${mapParameters.zoom},0/${mapParameters.width}x${mapParameters.height}?access_token=${accessToken}`
+    );
+    if (res.status === 200 || res.status === 201) {
+      const data = await res.arrayBuffer();
+      const base64Data = btoa(
+        new Uint8Array(data).reduce(
+          (data, byte) => data + String.fromCharCode(byte),
+          ""
+        )
+      );
+      console.log(map?.current?.getCenter());
+      const { lng, lat } = map?.current?.getCenter();
+      const zoom = map.current.getZoom();
+      console.log("Longitude:", lng);
+      console.log("Latitude:", lat);
+      console.log("Zoom:", zoom);
+      const objectURL = `data:image/jpeg;base64,${base64Data}`;
+      setImageURL(objectURL);
+    } else {
+      console.error("Error fetching image data");
+    }
+  };
   useEffect(() => {
     mapboxgl.accessToken = accessToken;
-    const map = new mapboxgl.Map({
-      container: "map-container",
+    const mapBox = new mapboxgl.Map({
+      container: mapContainer.current,
       style: "mapbox://styles/mapbox/streets-v12",
-      center: [73.8567, 18.5204],
+      center: [mapParameters.lon, mapParameters.lat],
       zoom: 10,
     });
+    if (!map.current) {
+      map.current = mapBox;
+    }
     const nav = new mapboxgl.NavigationControl();
-    map.addControl(nav, "top-right");
-    map.addControl(
+    mapBox.addControl(nav, "top-right");
+    mapBox.addControl(
       new mapboxgl.GeolocateControl({
         positionOptions: {
           enableHighAccuracy: true,
@@ -26,14 +63,18 @@ export const MapBox = () => {
     );
     const marker1 = new mapboxgl.Marker()
       .setLngLat([73.8562, 18.5204])
-      .addTo(map);
-    map.addControl(new mapboxgl.ScaleControl());
-
-    return () => map.remove();
+      .addTo(mapBox);
+    mapBox.addControl(new mapboxgl.ScaleControl());
+    console.log(map, mapContainer);
+    return () => mapBox.remove();
   }, []);
   return (
     <>
-      <div id="map-container" className="map" />
+      <div ref={mapContainer} id="map-container" className="map">
+        {/* <ReactMapGl></ReactMapGl> */}
+      </div>
+      <button onClick={() => fetchImage()}>Take a Screenshot</button>
+      {imageURL && <img src={imageURL} alt="API Fetched" />}
     </>
   );
 };
